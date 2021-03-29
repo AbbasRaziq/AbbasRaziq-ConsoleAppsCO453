c2561305a958cfa123bd9f68f0447a43df680cce
AbbasRaziq <78424813+AbbasRaziq@users.noreply.github.com>
2021-03-29 14:13:29 +0100

    Initial commit

---
+++ ConsoleAppProject/Helpers/EnumHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace ConsoleAppProject.Helpers
+{
+    /// <summary>
+    /// Found on Stack Overflow and works with any enunmeration
+    /// See example of how to use it in the Enuerations wiki page
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class EnumHelper<T>
+    {
+        public static string GetDescription(Enum @enum)
+        {
+            if (@enum == null)
+                return null;
+
+            string description = @enum.ToString();
+
+            try
+            {
+                FieldInfo fi = @enum.GetType().GetField(@enum.ToString());
+
+                DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attributes.Length > 0)
+                    description = attributes[0].Description;
+            }
+            catch
+            {
+            }
+
+            return description;
+        }
+
+        /// <summary>
+        /// Found on Stack Overflow and works with any enumeration
+        /// </summary>
+        public static string GetName(T value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+
+            var descriptionAttributes = fieldInfo.GetCustomAttributes(
+                typeof(DisplayAttribute), false) as DisplayAttribute[];
+
+            if (descriptionAttributes == null) return string.Empty;
+            return (descriptionAttributes.Length > 0) ? descriptionAttributes[0].Name : value.ToString();
+        }
+    }
+}
---
+++ ConsoleAppProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleAppProject.App03;
+using ConsoleAppProject.Helpers;
+using System;
+
+namespace ConsoleAppProject
+{
+    /// <summary>
+    /// The main method in this class is called first
+    /// when the application is started.  It will be used
+    /// to start Apps 01 to 05 for CO453 CW1
+    /// 
+    /// This Project has been modified by:
+    /// Derek Peacock 14/12/2020
+    /// </summary>
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            
+            Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
+            Console.WriteLine();
+
+            // Using an extension method for each enumeration
+            Console.WriteLine("Using MyEnum Extension Method!\n");
+            Console.WriteLine("MyEnum Value = " + MyEnum.FirstValue);
+            Console.WriteLine("MyEnum Friendly Value = " + MyEnum.FirstValue.EnumValue());
+            Console.WriteLine();
+
+            // Using an extension method for any enumeration
+            StudentGrades grades = new StudentGrades();
+            grades.TestGradesEnumeration();
+        }
+    }
+}
